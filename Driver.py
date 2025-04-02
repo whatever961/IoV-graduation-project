7a1a5e30cfd19b5ce875eb2b8e72689da46b85ea
notpotato <62131293+whatever961@users.noreply.github.com>
2025-04-02 21:10:08 +0800

    Update Driver.py

--- Driver.py
+++ Driver.py
@@ -1,1 +1,13 @@
+import func
 
+"""
+Use to adjust the sumo driving environment through traci.
+
+option format: It's a dictionary
+[{"slowDown": 減速多少},
+ {"safeDist": 重設安全距離},
+ {"aggresive": 設定配合度},
+		  ...]
+"""
+def adjustDrivingEnv(option):
+  pass
