37605b00fa20dc766cb7e735c5aa5f94678c0400
chen00-00 <sugarchenhs@gmail.com>
2025-04-03 22:46:12 +0800

    Change Driver.py

--- Driver.py
+++ Driver.py
@@ -3,11 +3,39 @@ import func
 """
 Use to adjust the sumo driving environment through traci.
 
-option format: It's a dictionary
-[{"slowDown": 減速多少},
- {"safeDist": 重設安全距離},
- {"aggresive": 設定配合度},
-		  ...]
+option format: It's a class
+
+class Vehicle:
+	def __init__(self, vehID, slowDownSpeed, slowDownDuration, safeDist, accel, decel, speedMode, maxSpeed, response):
+		self.vehID = vehID									: 車輛ID(string)
+		self.slowDown = [slowDownSpeed, slowDownDuration]	: [減速到達的速度, 減速所需時間](double, double)
+		self.safeDist = safeDist							: 重設安全距離(double)
+		self.accel = accel									: 加速度(double)
+		self.decel = decel									: 減速度(double)
+		self.speedMode = speedMode							: 行車模式(integer)[]
+		self.maxSpeed = maxSpeed							: 最大速度(double)
+		self.response = response							: 反應時間(double)
 """
+
+"""
+Bits setting for speedMode from right to left.
+
+bit0: Regard safe speed
+bit1: Regard maximum acceleration
+bit2: Regard maximum deceleration
+bit3: Regard right of way at intersections (only applies to approaching foe vehicles outside the intersection)
+bit4: Brake hard to avoid passing a red light
+bit5: Disregard right of way within intersections (only applies to foe vehicles that have entered the intersection).
+bit6: Disregard speed limit.
+"""
+
+# option 改用 class 的 list
 def adjustDrivingEnv(option):
-  pass
+	for i in option:
+		slowDown(i.vehID, i.slowDownSpeed, i.slowDownDuration)
+		setAccel(i.vehID, i.accel)
+		setDecel(i.vehID, i.decel)
+		setSpeedMode(i.vehID, i.speedMode)
+		setMaxSpeed(i.vehID, i.maxSpeed)
+		setMinGap(i.vehID, i.safeDist)
+		setTau(i.vehID, i.response)
