cf6086759f8108c6f083edaa3dd4cc9aad518984
notpotato <62131293+whatever961@users.noreply.github.com>
2025-05-04 01:59:00 +0800

    Update Simulation.py

--- Simulation.py
+++ Simulation.py
@@ -6,25 +6,24 @@ client.username_pw_set("rw", "readwrite")
 # Connect to the server(local)
 client.connect("127.0.0.1", 1883, 60)
 
-# Collision log
-collision_list = []
-collision_file = open("collide_log", "ra")
+if __name__ == "__main__":
+    collision_list = []
+    collision_file = open("collide_log", "ra")
 
-# Start simulation
-traci.start(["sumo-gui", "-c", "map.sumo.cfg"])
-while traci.simulation.getMinExpectedNumber()>0:
-    if traci.simulation.getCurrentTime()%900000==0: # Get new data every 15 minutes
-        # MQTT_pub to OBU
-        # Get data from Traci
-        msg = json.loads('{"ID": 12, "first_name": "Michael", "last_name": "Rodgers", "department": "Marketing"}')   # <- wait for fix
-        # Publish data to OBU
-        client.publish("cp", json.dumps(msg))
-    traci.simulation.step()
-    # If there's a car accident, record ID of cars
-    # if(traci.getCollidingVehiclesNumber()>0):
-        # collision_list.append(traci.getCollidingVehiclesIDList())
-traci.close()
+    #Start simulation
+    traci.start(["sumo-gui", "-c", "map.sumo.cfg"])
+    while traci.simulation.getMinExpectedNumber()>0:
+        #Split and distribute simulation data
 
-# Write log file base on whatever the fuck we got(not implement yet)
+        #Wait for all PCs ack back
 
-collision_file.close()
+        #Continue simulation
+        traci.simulation.step()
+        #If there's a car accident, record ID of cars
+        #if(traci.getCollidingVehiclesNumber()>0):
+            #collision_list.append(traci.getCollidingVehiclesIDList())
+    traci.close()
+
+    #Write log file base on whatever the fuck we got(not implement yet)
+
+    collision_file.close()
