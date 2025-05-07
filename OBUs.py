4bfa061400c6f6627c80db1824a514c95efc3db4
notpotato <62131293+whatever961@users.noreply.github.com>
2025-05-08 02:14:15 +0800

    Update OBUs.py

--- OBUs.py
+++ OBUs.py
@@ -2,6 +2,37 @@ import random
 import func
 import Strategy
 
+
+def infer_local_congestion(vehicles):
+    edge_speeds = {}
+    edge_counts = {}
+    edge_max_speeds = {}
+
+    for v in vehicles:
+        edge = v.get("road")
+        speed = v.get("speed")
+        max_speed = v.get("maxSpeed")  # fallback = 50 km/h
+
+        if edge not in edge_speeds:
+            edge_speeds[edge] = 0
+            edge_counts[edge] = 0
+            edge_max_speeds[edge] = max_speed
+
+        edge_speeds[edge] += speed
+        edge_counts[edge] += 1
+
+    congested_edges = []
+    for edge, count in edge_counts.items():
+        avg_speed = edge_speeds[edge] / count
+        max_speed = edge_max_speeds[edge]
+        threshold = max_speed * 0.7
+
+        if avg_speed < threshold and count >= 4:
+            congested_edges.append(edge)
+
+    return congested_edges
+
+
 # Callback when connected to the broker
 def on_connect(client, userdata, flags, rc, properties=None):
     print(f"[{args.pc_topic_id}] Connected to MQTT with result code {rc}")
@@ -18,15 +49,17 @@ def on_message(client, userdata, msg):
     except Exception as e:
         print(f"Failed to parse message: {e}")
         return
+    
+    congested_edges = infer_local_congestion(assigned_vehicles)
     option = None
     weather_factor = None
     if traci.getCurrentTime()%1200000==0:
         #cloud_data = fetchExternalData("https://opendata.cwa.gov.tw/api/v1/rest/datastore/O-A0003-001?Authorization=CWA-7B465ABE-F54D-4231-ABB4-D62EEFC1F684&format=JSON&StationId=466930,466910,466920,CAAH60,A0A460,AOA010,G2AI50&WeatherElement=Weather,VisibilityDescription,Now&GeoInfo=CountyName,TownName")
         #weather_factor = OBUProcessData(cloud_data)
-        weather_factor = random.choice([-0.07, -0.13, -0.11, -0.09])
+        weather_factor = random.choice([-0.05, -0.07, -0.13, -0.11, -0.09]) #for demo usage, change the weather state randomly
         option = setOption(weather_factor, veh_ids)
     ack_topic = f"controller/ack/pc{args.pc_topic_id}"
-    client.publish(ack_topic, json.dumps({"option": option, "weather_factor": weather_factor}))
+    client.publish(ack_topic, json.dumps({"option": option, "weather_factor": weather_factor, "congested_edges": congested_edges}))
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--pc_topic_id', type=int, required=True, help="Topic for distributed OBUs' PC (e.g. pc0, pc1)")
