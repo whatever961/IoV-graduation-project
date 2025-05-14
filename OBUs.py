8143443827a7ef1b7f2818e0e68cb8ccb5cf9aff
notpotato <62131293+whatever961@users.noreply.github.com>
2025-05-14 16:45:53 +0800

    Update OBUs.py

--- OBUs.py
+++ OBUs.py
@@ -1,5 +1,15 @@
+import traci
+import requests
+import os
+import csv
+import json
+import xml.etree.ElementTree as ET
+import pandas as pd
+from io import StringIO
+import paho.mqtt.client as mqtt
+import time
 import random
-import func
+import argparse
 import Strategy
 
 
@@ -57,7 +67,7 @@ def on_message(client, userdata, msg):
         #cloud_data = fetchExternalData("https://opendata.cwa.gov.tw/api/v1/rest/datastore/O-A0003-001?Authorization=CWA-7B465ABE-F54D-4231-ABB4-D62EEFC1F684&format=JSON&StationId=466930,466910,466920,CAAH60,A0A460,AOA010,G2AI50&WeatherElement=Weather,VisibilityDescription,Now&GeoInfo=CountyName,TownName")
         #weather_factor = OBUProcessData(cloud_data)
         weather_factor = 0 # 這我不知道怎麼設定，反正晴天雨天
-        option = setOption(weather_factor, veh_ids, congested_edges)
+        option = Strategy.setOption(weather_factor, veh_ids, congested_edges)
     ack_topic = f"controller/ack/pc{args.pc_topic_id}"
     client.publish(ack_topic, json.dumps({"option": option}))
 
