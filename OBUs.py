d88ad279e3184f6a86d5791a9db2c2c33b2fd7be
notpotato <62131293+whatever961@users.noreply.github.com>
2025-05-18 15:15:11 +0800

    Update OBUs.py

--- OBUs.py
+++ OBUs.py
@@ -54,19 +54,23 @@ def on_message(client, userdata, msg):
     # Parse incoming vehicle data
     try:
         assigned_vehicles = json.loads(msg.payload.decode())
-        current_time = 0
     except Exception as e:
         print(f"Failed to parse message: {e}")
         return
     
-    congested_edges = infer_local_congestion(assigned_vehicles)
+    congested_edges = assigned_vehicles[0].get("congested_edge")
+    current_time = assigned_vehicles[0].get("current_time")
     option = []
     weather_factor = None
     if current_time%1200.0==0.0:
         #cloud_data = fetchExternalData("https://opendata.cwa.gov.tw/api/v1/rest/datastore/O-A0003-001?Authorization=CWA-7B465ABE-F54D-4231-ABB4-D62EEFC1F684&format=JSON&StationId=466930,466910,466920,CAAH60,A0A460,AOA010,G2AI50&WeatherElement=Weather,VisibilityDescription,Now&GeoInfo=CountyName,TownName")
         #weather_factor = OBUProcessData(cloud_data)
-        weather_factor = 0 # 這我不知道怎麼設定，反正晴天雨天
+        weather_factor = 0 # 晴天(0) 雨天(-0.15)
         option = Strategy.setOption(weather_factor, assigned_vehicles, congested_edges)
+    else if current_time%3.0==0.0:
+        weather_factor = 0 # for reroute
+        option = Strategy.setOption(weather_factor, assigned_vehicles, congested_edges)
+        
     ack_topic = f"controller/ack/pc{args.pc_topic_id}"
     client.publish(ack_topic, json.dumps({"option": option}, cls=func.AdvancedJSONEncoder))
 
