0169a4caef4f5fe136f9129dc17a4d4d96f23107
notpotato <62131293+whatever961@users.noreply.github.com>
2025-07-14 16:10:41 +0800

    Update OBUs.py

--- OBUs.py
+++ OBUs.py
@@ -65,13 +65,10 @@ def on_message(client, userdata, msg):
         #cloud_data = fetchExternalData("https://opendata.cwa.gov.tw/api/v1/rest/datastore/O-A0003-001?Authorization=CWA-7B465ABE-F54D-4231-ABB4-D62EEFC1F684&format=JSON&StationId=466930,466910,466920,CAAH60,A0A460,AOA010,G2AI50&WeatherElement=Weather,VisibilityDescription,Now&GeoInfo=CountyName,TownName")
         #weather_factor = OBUProcessData(cloud_data)
         weather_factor = 0 # 晴天(0) 雨天(-0.15)
-        option = Strategy.setOption(weather_factor, assigned_vehicles)
-    elif current_time%10.0==0.0:
-        weather_factor = 0 # for reroute
-        option = Strategy.setOption(weather_factor, assigned_vehicles)
-        
-    ack_topic = f"controller/ack/pc{args.pc_topic_id}"
-    client.publish(ack_topic, json.dumps({"option": option}, cls=func.AdvancedJSONEncoder))
+        for vehicle in assigned_vehicles:
+            option = Strategy.setOption(weather_factor, assigned_vehicles)
+            ack_topic = f"controller/ack/pc{args.pc_topic_id}"
+            client.publish(ack_topic, json.dumps({"option": option}, cls=func.AdvancedJSONEncoder))
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--pc_topic_id', type=int, required=True, help="Topic for distributed OBUs' PC. Insert integer >= 0 (e.g. pc{0}, pc{1})")
