877b2f92260d1934115ea8bcc52733349a435605
notpotato <62131293+whatever961@users.noreply.github.com>
2025-07-18 20:04:35 +0800

    Update OBUs.py

--- OBUs.py
+++ OBUs.py
@@ -61,14 +61,13 @@ def on_message(client, userdata, msg):
     current_time = assigned_vehicles[0].get("current_time")
     option = []
     weather_factor = None
-    if current_time%1200.0==0.0:
-        #cloud_data = fetchExternalData("https://opendata.cwa.gov.tw/api/v1/rest/datastore/O-A0003-001?Authorization=CWA-7B465ABE-F54D-4231-ABB4-D62EEFC1F684&format=JSON&StationId=466930,466910,466920,CAAH60,A0A460,AOA010,G2AI50&WeatherElement=Weather,VisibilityDescription,Now&GeoInfo=CountyName,TownName")
-        #weather_factor = OBUProcessData(cloud_data)
-        weather_factor = 0 # 晴天(0) 雨天(-0.15)
-        for vehicle in assigned_vehicles:
-            option = Strategy.setOption(weather_factor, vehicle)
-            ack_topic = f"controller/ack/pc{args.pc_topic_id}"
-            client.publish(ack_topic, json.dumps({"option": option}, cls=func.AdvancedJSONEncoder))
+    #cloud_data = fetchExternalData("https://opendata.cwa.gov.tw/api/v1/rest/datastore/O-A0003-001?Authorization=CWA-7B465ABE-F54D-4231-ABB4-D62EEFC1F684&format=JSON&StationId=466930,466910,466920,CAAH60,A0A460,AOA010,G2AI50&WeatherElement=Weather,VisibilityDescription,Now&GeoInfo=CountyName,TownName")
+    #weather_factor = OBUProcessData(cloud_data)
+    weather_factor = 0 # 晴天(0) 雨天(-0.15)
+    for vehicle in assigned_vehicles:
+        option = Strategy.setOption(weather_factor, vehicle)
+        ack_topic = f"controller/ack/pc{args.pc_topic_id}"
+        client.publish(ack_topic, json.dumps({"option": option}, cls=func.AdvancedJSONEncoder))
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--pc_topic_id', type=int, required=True, help="Topic for distributed OBUs' PC. Insert integer >= 0 (e.g. pc{0}, pc{1})")
